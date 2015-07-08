18607ee8a84c9f2bdd72d4ce7a93a82a1c67f473
Víctor Pérez Tapia <victor@aidiapp.com>
2015-07-08 08:08:17 +0200

    Dialog enhancement
    Modifico el dialog para que sea fixed

--- js/loadingdialog.js
+++ js/loadingdialog.js
@@ -1,24 +1,25 @@
 // JavaScript Document
 var LoadingDialog={
 init:function(){
-	console.log("Inicializamos el LoadingDialog");
-	jQuery('article section').prepend('<div class="loading"></div>');
-	this.hide();	
-	this.manejadores();
+    console.log("Inicializamos el LoadingDialog");
+    jQuery('article').prepend('<div class="loading"></div>');
+    this.hide();
+    this.manejadores();
 },
 manejadores:function(){
-	
+    
 },
 show:function(texto){
-	console.log(";ostramos el dialogo");
-	$('article.current .loading').html(texto).addClass('visible');;
-	$('article.current .loading').nextAll().addClass('whenloading');
-    $('section.current').scrollTop(0);
+    console.log(";ostramos el dialogo");
+    //$('section.current').scrollTop(0);
+    $('article.current .loading').html(texto).addClass('visible');;
+    $('article.current .loading').nextAll().addClass('whenloading');
+    
 },
 hide:function(t){
-	console.log("Ocultamos");
-	$('article.current .loading').delay(t).removeClass('visible');	
-	$('.whenloading').removeClass('whenloading');
+    console.log("Ocultamos");
+    $('article.current .loading').delay(t).removeClass('visible');
+    $('.whenloading').removeClass('whenloading');
 }
-	
+    
 }
